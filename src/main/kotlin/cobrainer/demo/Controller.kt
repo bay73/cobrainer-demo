cc8ed67dcacd8a285e981683786bd41b90767821
AndreyBogdanov <andrey.bogdanov@cobrainer.com>
2025-05-09 12:15:35 +0200

    Remove empty file

--- src/main/kotlin/cobrainer/demo/Controller.kt
+++ src/main/kotlin/cobrainer/demo/Controller.kt
@@ -1,16 +1,23 @@
 package cobrainer.demo
 
+import cobrainer.demo.model.JobArchitectureItemId
 import cobrainer.demo.service.JobArchitectureService
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping(
+    produces = [MediaType.APPLICATION_JSON_VALUE],
+)
 class Controller(
     private val service: JobArchitectureService
 ) {
-
-    @GetMapping("/job-artchitecture", consumes = ["application/json"])
+    @GetMapping("/job-architecture/{jobArchitectureId}")
     fun getArchitecture(
+        @PathVariable jobArchitectureId: JobArchitectureItemId,
     ) {
     }
 }
